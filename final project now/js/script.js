5a357dedc5e5b61e77ab54144bab5b39672735fa
nathandkliewer <nathankwebdesign.com@gmail.com>
2018-05-16 15:41:22 -0700

    finals
    for the finals.

---
+++ final project now/js/script.js
@@ -0,0 +1,38 @@
+// set up passWord
+
+//var pw = prompt("What is the password?","");
+//
+// pw (age);
+// if (age = 1969) {
+
+
+// enter Custmer 1st and last name
+// var cName = prompt(cName)
+
+
+
+function myFunction() {
+    var x = document.getElementById("cName").value;
+
+      document.getElementById("c1name").innerHTML = x;
+
+
+}
+
+
+
+
+
+
+
+
+// enter date receive Quote
+<p id="c1name"></p>
+
+<div id = >
+
+// enter date send quote  when marked it goes to Completed
+
+
+
+//Completed quote
